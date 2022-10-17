a8d309641ee020cc9aa0bc80151ef96fc633a992
Dmitry <suprun1212@gmail.com>
2022-10-17 14:47:35 +0300

    Initial commit

---
+++ Instagram/Instagram/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Instagram
+//
+//  Created by Дмитрий Супрун on 17.10.22.
+//
+
+import UIKit
+
+/// SceneDelegate
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+    }
+}
---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Дмитрий Супрун on 17.10.22.
+//
+
+import UIKit
+
+/// ViewController
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
