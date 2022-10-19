bee138bb278c64b9d5a8c0a60d96c8d3e297fb1e
Dmitry <suprun1212@gmail.com>
2022-10-19 22:45:26 +0300

    Fixed issue

--- Instagram/Instagram/MainFeedViewController.swift
+++ Instagram/Instagram/MainFeedViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-/// Main Feed. Present new posts, recommendation, story feed
+/// Present new posts, recommendation, story feed
 final class MainFeedViewController: UIViewController {
     // MARK: - Constants
     enum Constants {
@@ -28,7 +28,6 @@ final class MainFeedViewController: UIViewController {
         super.viewDidLoad()
         setupRefreshControl()
         addViews()
-//        posts = loadPostsFeed()
     }
     
     // MARK: - Objc Private Methods
@@ -47,10 +46,6 @@ final class MainFeedViewController: UIViewController {
     }
 }
 
-// MARK: - UITableViewDelegate
-extension MainFeedViewController: UITableViewDelegate {
-}
-
 // MARK: - UITableViewDataSource
 
 extension MainFeedViewController: UITableViewDataSource {
@@ -66,67 +61,5 @@ extension MainFeedViewController: UITableViewDataSource {
         default: cell = tableView.dequeueReusableCell(withIdentifier: Constants.postCellID, for: indexPath)
         }
         return cell
-    }
-}
-
-// MARK: - Service Layer
-private extension MainFeedViewController {
-    func loadPostsFeed() -> [Post] {
-        let posts = [Post(userID: 1,
-                          images: ["food01",
-                                   "food02",
-                                   "food03",
-                                  ],
-                          subTitleText: "",
-                          isLiked: true,
-                          isSavedToCollection: true,
-                          likesCount: 122,
-                          descriptionText: "",
-                          postDate: Date(timeIntervalSince1970: 1665099000)),
-                     Post(userID: 2,
-                          images: ["food04",
-                                   "food05",
-                                   "food06",
-                                  ],
-                          subTitleText: "",
-                          isLiked: false,
-                          isSavedToCollection: true,
-                          likesCount: 132,
-                          descriptionText: "",
-                          postDate: Date(timeIntervalSince1970: 1665089000)),
-                     Post(userID: 3,
-                          images: ["food07",
-                                   "food08",
-                                   "food09",
-                                  ],
-                          subTitleText: "",
-                          isLiked: false,
-                          isSavedToCollection: false,
-                          likesCount: 37,
-                          descriptionText: "",
-                          postDate: Date(timeIntervalSince1970: 1665079000)),
-                     Post(userID: 4,
-                          images: ["food10",
-                                   "food11",
-                                   "food12",
-                                  ],
-                          subTitleText: "",
-                          isLiked: false,
-                          isSavedToCollection: false,
-                          likesCount: 7340,
-                          descriptionText: "",
-                          postDate: Date(timeIntervalSince1970: 1665069000)),
-        ]
-        return posts
-    }
-    
-    func loadUserInfo(userID: Int) -> User? {
-        switch userID {
-        case 1: return User(name: "Jaahi", id: 1, avatarImageName: "1")
-        case 2: return User(name: "Nedras", id: 2, avatarImageName: "2")
-        case 3: return User(name: "Mahtan", id: 3, avatarImageName: "3")
-        case 4: return User(name: "Kleastera", id: 4, avatarImageName: "4")
-        default: return nil
-        }
     }
 }
